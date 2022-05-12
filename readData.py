055a11cadca00239c4de9d4ba69d303635a1532d
Saar Nir <bizmazt@gmail.com>
2022-05-12 14:19:09 +0300

    update

--- readData.py
+++ readData.py
@@ -7,13 +7,25 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
 }
 
-req = requests.get("https://www.ims.tau.ac.il/Tal/Syllabus/Syllabus_L.aspx?course=0366110101&year=2020", headers=headers)
 
-soup = BeautifulSoup(req.content, "html.parser")
 
-res = soup.find("div", {"id" : "div_dataToc"})
-
+table_res = res.find("table")
 res2 = soup.find('p').get_text()
+
+
+
+def createURL(course_id):
+    return "https://www.ims.tau.ac.il/Tal/Syllabus/Syllabus_L.aspx?course="+course_id+"01&year=2021"
+
+def getIDS(soup_res):
+    req = requests.get("https://exact-sciences.tau.ac.il/yedion/shar_ruach", headers=headers)
+
+    soup = BeautifulSoup(req.content, "html.parser")
+
+    res = soup.find("div", {"id": "node-12227"})
+
+def getName(soup_res):
+
 # for tag in soup.findAll('p'):
 #     print(tag.get_text())
 
@@ -29,11 +41,11 @@ def cleanhtml(text):
             continue
         if char == '>':
             flag = False
-            new_str += "\n"
+            # new_str += "\n"
             continue
         if not flag:
             new_str += char
     return new_str
 
 
-print(cleanhtml(res))
+print(cleanhtml(table_res))
